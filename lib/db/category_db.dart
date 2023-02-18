b0db46f90c6b175d1d114c89773c2323548367c8
Muhammed <muhammednihalit@gmail.com>
2023-02-18 15:52:25 +0530

    till part 18

---
+++ lib/db/category_db.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_application_1/model/category/category_model.dart';
+import 'package:hive_flutter/adapters.dart';
+
+const categorydbname = 'databasename';
+
+abstract class categorydbfunctions {
+  Future<List<categoryModel>> getcategories();
+  Future<void> insertcategory(categoryModel value);
+}
+
+class categorydb implements categorydbfunctions {
+  @override
+  Future<void> insertcategory(categoryModel value) async {
+    final categorydb = await Hive.openBox<categoryModel>(categorydbname);
+    await categorydb.add(value);
+  }
+
+  @override
+  Future<List<categoryModel>> getcategories() async {
+    final categorydb = await Hive.openBox<categoryModel>(categorydbname);
+    return categorydb.values.toList();
+  }
+}
---
+++ lib/model/category/category_model.dart
@@ -0,0 +1,30 @@
+import 'package:hive_flutter/adapters.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+part 'category_model.g.dart';
+
+@HiveType(typeId:2)
+enum categoryType {
+  @HiveField(0)
+  income,
+  @HiveField(1)
+  expense,
+}
+  
+@HiveType(typeId: 1)
+class categoryModel {
+  @HiveField(0)
+  final String id;
+  @HiveField(1)
+  final String name;
+  @HiveField(2)
+  final bool isdeleted;
+  @HiveField(3)
+  final categoryType type;
+
+  categoryModel({
+    required this.id,
+    required this.name,
+    required this.type,
+    this.isdeleted = false,
+  });
+}
